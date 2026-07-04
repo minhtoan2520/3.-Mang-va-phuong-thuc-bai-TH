1e719f1b167b213eda1ffed83a310022ea6ac707
minhtoan <minhtoan2520@gmail.com>
2026-07-04 22:27:35 +0700

    update

--- NumberArrayMin.java
+++ NumberArrayMin.java
@@ -1,8 +1,10 @@
+
+//TH tim gia tri nho nhat trong mang su dung phuong thuc
 public class NumberArrayMin {
     public static void main(String[] args) {
         int[] arr = { 4, 12, 7, 8, 1, 6, 9 };
         int index = minValue(arr);
-        System.out.println("The smallest element in the array is: " + arr[index]);
+        System.out.println("The smallest element in the array is: " + arr[index] + " at position " + index);
     }
 
     public static int minValue(int[] array) {
